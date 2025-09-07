e1bca1efb667cd9ab9cb0cdcc60467b99a36a602
youthk1121 <youth.k.19981121@gmail.com>
2025-09-07 17:10:30 +0900

    #3 フォーク数が取得できない問題を修正
    パースする文字列が間違っていた

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
@@ -50,7 +50,7 @@ class OneViewModel(application: Application) : AndroidViewModel(application) {
                 val language = jsonItem.optString("language")
                 val stargazersCount = jsonItem.optLong("stargazers_count")
                 val watchersCount = jsonItem.optLong("watchers_count")
-                val forksCount = jsonItem.optLong("forks_conut")
+                val forksCount = jsonItem.optLong("forks_count")
                 val openIssuesCount = jsonItem.optLong("open_issues_count")
 
                 items.add(
