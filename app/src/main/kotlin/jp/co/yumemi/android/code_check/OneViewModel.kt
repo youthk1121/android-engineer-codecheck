2fda8b88f39312abea564ae6041b4e63243a037f
youthk1121 <youth.k.19981121@gmail.com>
2025-09-07 17:25:12 +0900

    #3 通信がエラーになった時にクラッシュする問題を修正

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
@@ -5,6 +5,7 @@ package jp.co.yumemi.android.code_check
 
 import android.app.Application
 import android.os.Parcelable
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import io.ktor.client.*
 import io.ktor.client.call.*
@@ -16,6 +17,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import kotlinx.parcelize.Parcelize
+import okio.IOException
 import org.json.JSONObject
 import java.util.*
 
@@ -29,9 +31,14 @@ class OneViewModel(application: Application) : AndroidViewModel(application) {
         val client = HttpClient(Android)
 
         return@runBlocking GlobalScope.async {
-            val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
-                header("Accept", "application/vnd.github.v3+json")
-                parameter("q", inputText)
+            val response: HttpResponse = try {
+                client.get("https://api.github.com/search/repositories") {
+                    header("Accept", "application/vnd.github.v3+json")
+                    parameter("q", inputText)
+                }
+            } catch (e: IOException) {
+                Log.e("検索結果", "検索エラー", e)
+                return@async emptyList()
             }
 
             val jsonBody = JSONObject(response.receive<String>())
