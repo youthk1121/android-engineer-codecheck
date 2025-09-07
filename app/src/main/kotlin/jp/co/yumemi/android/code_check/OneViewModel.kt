e9f27e63a9b449effe295b2106f630e7f50a24b8
youthk1121 <youth.k.19981121@gmail.com>
2025-09-07 21:32:24 +0900

    #5 検索処理をRepositoryに分離

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneViewModel.kt
@@ -5,59 +5,30 @@ package jp.co.yumemi.android.code_check
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.android.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import org.json.JSONObject
+import jp.co.yumemi.android.code_check.repository.SearchRepository
 
 /**
  * OneFragment で使う
  */
 class OneViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val searchRepository = SearchRepository()
+
     // 検索結果
     suspend fun searchResults(inputText: String): List<Item> {
-        val client = HttpClient(Android)
-
-        val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
-                header("Accept", "application/vnd.github.v3+json")
-                parameter("q", inputText)
-            }
-
-        val jsonBody = JSONObject(response.receive<String>())
-
-        val jsonItems = jsonBody.optJSONArray("items") ?: return emptyList()
-
-        val items = mutableListOf<Item>()
-
-        /**
-         * アイテムの個数分ループする
-         */
-        for (i in 0 until jsonItems.length()) {
-            val jsonItem = jsonItems.optJSONObject(i)
-            val name = jsonItem.optString("full_name")
-            val ownerIconUrl = jsonItem.optJSONObject("owner")?.optString("avatar_url")
-            val language = jsonItem.optString("language")
-            val stargazersCount = jsonItem.optLong("stargazers_count")
-            val watchersCount = jsonItem.optLong("watchers_count")
-            val forksCount = jsonItem.optLong("forks_count")
-            val openIssuesCount = jsonItem.optLong("open_issues_count")
-
-            items.add(
-                Item(
-                    name = name,
-                    ownerIconUrl = ownerIconUrl,
-                    language = getApplication<Application>().getString(R.string.written_language, language),
-                    stargazersCount = stargazersCount,
-                    watchersCount = watchersCount,
-                    forksCount = forksCount,
-                    openIssuesCount = openIssuesCount
-                )
+        return searchRepository.search(inputText).map { responseItem ->
+            Item(
+                name = responseItem.name,
+                ownerIconUrl = responseItem.ownerIconUrl,
+                language = getApplication<Application>().getString(
+                    R.string.written_language,
+                    responseItem.language
+                ),
+                stargazersCount = responseItem.stargazersCount,
+                watchersCount = responseItem.watchersCount,
+                forksCount = responseItem.forksCount,
+                openIssuesCount = responseItem.openIssuesCount,
             )
         }
-
-        return items.toList()
     }
 }
